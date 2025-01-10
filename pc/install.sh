5aa7f6b5507cec6951dca6ac882e365546160662
Dok6n <ehrbs2018@gmail.com>
2025-01-10 12:06:05 +0900

    for macos

---
+++ pc/install.sh
@@ -0,0 +1,21 @@
+#   install list for macOS
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" # homebrew
+
+#   mac apps
+brew install --cask karabiner-elements # right_command - f19
+brew install --cask scroll-reverser
+brew install --cask rectangle
+brew install --cask logitech-g-hub
+
+#   for develop
+brew install --cask item2
+brew install --cask cursor
+brew install --cask postman
+
+brew install nvm
+brew install nvim
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+brew install fzf
+brew install eza
+brew install z
